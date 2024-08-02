7455882466d6dc8d7c270cf721f31b6be7de9c3e
Nikita Voloshenko <stivens54@gmail.com>
2024-08-01 22:10:01 -0700

    add greedy stub

--- greedy.py
+++ greedy.py
@@ -1,9 +1,17 @@
 import sys
 
+from helper import euclidean_distance, calculate_route_distance, parse_input
+
+def greedy_insertion(loads):
+    return [], 0
+
 def main():
     if len(sys.argv) != 2:
         return
 
+    file_path = sys.argv[1]
+    loads = parse_input(file_path)
+    drivers, total_cost = greedy_insertion(loads)
 
 if __name__ == "__main__":
     main()
